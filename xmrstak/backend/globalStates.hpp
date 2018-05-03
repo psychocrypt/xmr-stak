920b32701687179513c568e89551253655565d94
psychocrypt <psychocryptHPC@gmail.com>
2018-05-03 20:35:56 +0200

    add read write lock class
    add log class from Will Zhang:
    
    Package: cpputil
    Source: https://github.com/willzhang4a58/cpputil
    License: MIT License

--- xmrstak/backend/globalStates.hpp
+++ xmrstak/backend/globalStates.hpp
@@ -5,6 +5,7 @@
 #include "xmrstak/misc/console.hpp"
 
 #include <atomic>
+#include <condition_variable>
 
 constexpr static size_t invalid_pool_id = (-1);
 
@@ -20,6 +21,61 @@ struct pool_data
 	{
 	}
 };
+
+
+class RWLock {
+public:
+    RWLock() : _status(0), _waiting_readers(0), _waiting_writers(0) {}
+    RWLock(const RWLock&) = delete;
+    RWLock(RWLock&&) = delete;
+    RWLock& operator = (const RWLock&) = delete;
+    RWLock& operator = (RWLock&&) = delete;
+
+    void rdlock() {
+        std::unique_lock<std::mutex> lck(_mtx);
+        _waiting_readers += 1;
+        _read_cv.wait(lck, [&]() { return _waiting_writers == 0 && _status >= 0; });
+        _waiting_readers -= 1;
+        _status += 1;
+    }
+
+    void wrlock() {
+        std::unique_lock<std::mutex> lck(_mtx);
+        _waiting_writers += 1;
+        _write_cv.wait(lck, [&]() { return _status == 0; });
+        _waiting_writers -= 1;
+        _status = -1;
+    }
+
+    void unlock() {
+        std::unique_lock<std::mutex> lck(_mtx);
+        if (_status == -1) {
+            _status = 0;
+        } else {
+            _status -= 1;
+        }
+        if (_waiting_writers > 0) {
+            if (_status == 0) {
+                _write_cv.notify_one();
+            }
+        } else {
+            _read_cv.notify_all();
+        }
+    }
+
+private:
+    // -1    : one writer
+    // 0     : no reader and no writer
+    // n > 0 : n reader
+    int32_t _status;
+    int32_t _waiting_readers;
+    int32_t _waiting_writers;
+    std::mutex _mtx;
+    std::condition_variable _read_cv;
+    std::condition_variable _write_cv;
+};
+
+
 
 struct globalStates
 {
@@ -55,6 +111,8 @@ private:
 	globalStates() : iThreadCount(0), iGlobalJobNo(0), iConsumeCnt(0)
 	{
 	}
+
+	RWLock jobLock;
 };
 
 } // namespace xmrstak
