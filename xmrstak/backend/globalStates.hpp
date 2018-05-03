10b42a5531863408a12e7c7e361bf1e05365aa6d
psychocrypt <psychocryptHPC@gmail.com>
2018-05-03 20:40:49 +0200

    use read write locks to secure job updates
    user read write locks to be sure that no job is consumend during the job update

--- xmrstak/backend/globalStates.hpp
+++ xmrstak/backend/globalStates.hpp
@@ -3,9 +3,9 @@
 #include "miner_work.hpp"
 #include "xmrstak/misc/environment.hpp"
 #include "xmrstak/misc/console.hpp"
+#include "xmrstak/cpputil/read_write_lock.h"
 
 #include <atomic>
-#include <condition_variable>
 
 constexpr static size_t invalid_pool_id = (-1);
 
@@ -21,61 +21,6 @@ struct pool_data
 	{
 	}
 };
-
-
-class RWLock {
-public:
-    RWLock() : _status(0), _waiting_readers(0), _waiting_writers(0) {}
-    RWLock(const RWLock&) = delete;
-    RWLock(RWLock&&) = delete;
-    RWLock& operator = (const RWLock&) = delete;
-    RWLock& operator = (RWLock&&) = delete;
-
-    void rdlock() {
-        std::unique_lock<std::mutex> lck(_mtx);
-        _waiting_readers += 1;
-        _read_cv.wait(lck, [&]() { return _waiting_writers == 0 && _status >= 0; });
-        _waiting_readers -= 1;
-        _status += 1;
-    }
-
-    void wrlock() {
-        std::unique_lock<std::mutex> lck(_mtx);
-        _waiting_writers += 1;
-        _write_cv.wait(lck, [&]() { return _status == 0; });
-        _waiting_writers -= 1;
-        _status = -1;
-    }
-
-    void unlock() {
-        std::unique_lock<std::mutex> lck(_mtx);
-        if (_status == -1) {
-            _status = 0;
-        } else {
-            _status -= 1;
-        }
-        if (_waiting_writers > 0) {
-            if (_status == 0) {
-                _write_cv.notify_one();
-            }
-        } else {
-            _read_cv.notify_all();
-        }
-    }
-
-private:
-    // -1    : one writer
-    // 0     : no reader and no writer
-    // n > 0 : n reader
-    int32_t _status;
-    int32_t _waiting_readers;
-    int32_t _waiting_writers;
-    std::mutex _mtx;
-    std::condition_variable _read_cv;
-    std::condition_variable _write_cv;
-};
-
-
 
 struct globalStates
 {
@@ -112,7 +57,7 @@ private:
 	{
 	}
 
-	RWLock jobLock;
+	::cpputil::RWLock jobLock;
 };
 
 } // namespace xmrstak
